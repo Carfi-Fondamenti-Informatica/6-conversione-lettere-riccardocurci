c6bb8c8949536acb2799c2ea08a53ce34fa20099
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-30 13:29:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,17 @@
-//
-// Created by ricca on 29/11/2021.
-//
-
 #include "lib.h"
+bool conversione (char &a) {
+bool t=true;
+  if(a>=65 and a<=90 or a>=97 and a<=122){
+     t=true;
+     if(a>=65 and a<=90){
+      a=a+32;
+      return a;
+    }else{
+    a=a-32;
+    return a;
+    }
+  }else{
+    t=false;
+  }
+  return t;
+}
