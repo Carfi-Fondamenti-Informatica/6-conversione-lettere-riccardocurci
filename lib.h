9715a650f743ae14e5c00d371dcf1edce0fb4c75
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-30 13:08:37 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,9 +2,9 @@
 // Created by ricca on 29/11/2021.
 //
 
-#ifndef LEZIONE ESERCITAZIONE_6_LIBR_H
-#define LEZIONE ESERCITAZIONE_6_LIB_H
+#ifndef ESERCITAZIONE_6_LIBR_H
+#define ESERCITAZIONE_6_LIB_H
 
-#endif //LEZIONE //ESERCITAZIONE6_LIB_H
+#endif //ESERCITAZIONE6_LIB_H
 
 bool conversione (char&);
