e572684bd46de8a9d86881b02ceef48dd8e03ad5
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-30 13:08:07 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by ricca on 29/11/2021.
+//
 
+#ifndef LEZIONE ESERCITAZIONE_6_LIBR_H
+#define LEZIONE ESERCITAZIONE_6_LIB_H
+
+#endif //LEZIONE //ESERCITAZIONE6_LIB_H
+
+bool conversione (char&);
