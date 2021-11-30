4ce86d8286fa74d862ea28f14147ce7dc523adce
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-30 13:18:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+bool conversione (char &a) {
+bool t=true;
+  if(a>=65 and a<=90 or a>=97 and a<=122){
+    t=true;
+    if(a>=65 and a<=90 ){
+      a=a+32;
+      return a;
+      }
+    }else{
+    a=a-32 
+    return a;
+    }
+  }else{
+    t=false;
+  }
+  return t;
 }
