db7c79690110ce029484748a310db8deda291eec
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-30 13:38:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,9 @@
 #include "lib.h"
 bool conversione (char &a) {
 bool t=true;
-  if(a>=65 and a<=90 or a>=97 and a<=122){
+  if (a>=65 and a<=90 or a>=97 and a<=122){
      t=true;
-     if(a>=65 and a<=90){
+     if (a>=65 and a<=90){
       a=a+32;
       return a;
     }else{
